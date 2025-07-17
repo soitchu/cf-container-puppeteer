2c3ea5b2d8b46c5619baebdff03404f0a00a960b
soitchu <soitchu@users.noreply.github.com>
2025-07-16 23:11:32 -0700

    Test

--- container_src/index.js
+++ container_src/index.js
@@ -1,31 +1,31 @@
 import express from "express";
-
+import puppeteer from "puppeteer";
 const app = express();
 const PORT = 8080;
 
-app.get("/screenshot/:url", (req, res) => {
+app.get("/screenshot/:url", async (req, res) => {
   try {
     const url = req.params.url;
 
     try {
-      (async () => {
-        // const browser = await puppeteer.launch({});
-        // // const page = await browser.newPage();
-        // // await page.goto(`https://${url}`);
-        // // const screenshot = await page.screenshot();
-        // await browser.close();
+      const browser = await puppeteer.launch({});
+      const page = await browser.newPage();
+      await page.setViewport({ width: 400, height: 400 });
+      await page.goto(`https://${url}`);
 
-        // res.setHeader("Content-Type", "image/png");
-        // res.send(screenshot);
-      })();
+      const content = await page.content();
+
+      res.status(200).send(content);
+      return;
     } catch (error) {
       console.error("Error launching Puppeteer:", error);
+      res.status(500).send("Error launching Puppeteer: " + error);
+      return;
     }
-
-    res.send("test");
   } catch (error) {
     console.error("Error taking screenshot:", error);
-    res.status(500).send("Error taking screenshot: " + error);
+    // res.status(500).send("Error taking screenshot: " + error);
+    return;
   }
 });
 
