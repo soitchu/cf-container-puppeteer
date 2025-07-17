16cfdf27b31d717d7b4530eac2c87fe8ae8c7cf2
soitchu <soitchu@users.noreply.github.com>
2025-07-16 22:50:05 -0700

    Update route

--- container_src/index.js
+++ container_src/index.js
@@ -5,17 +5,22 @@ const app = express();
 const PORT = 8080;
 
 app.get("/screenshot/:url", (req, res) => {
-  const url = req.params.url;
-  (async () => {
-    const browser = await puppeteer.launch({});
-    const page = await browser.newPage();
-    await page.goto(`https://${url}`);
-    const screenshot = await page.screenshot();
-    await browser.close();
+  try {
+    const url = req.params.url;
+    (async () => {
+      const browser = await puppeteer.launch({});
+      const page = await browser.newPage();
+      await page.goto(`https://${url}`);
+      const screenshot = await page.screenshot();
+      await browser.close();
 
-    res.setHeader("Content-Type", "image/png");
-    res.send(screenshot);
-  })();
+      res.setHeader("Content-Type", "image/png");
+      res.send(screenshot);
+    })();
+  } catch (error) {
+    console.error("Error taking screenshot:", error);
+    res.status(500).send("Error taking screenshot: " + error);
+  }
 });
 
 app.listen(PORT, () => {
