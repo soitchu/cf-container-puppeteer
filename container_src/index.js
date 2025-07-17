3a83de06f9461f6bdfdd0338807ea203ee67baca
soitchu <soitchu@users.noreply.github.com>
2025-07-17 01:52:31 -0700

    Re-init browser

--- container_src/index.js
+++ container_src/index.js
@@ -3,21 +3,29 @@ import puppeteer from "puppeteer-core";
 const app = express();
 const PORT = 8080;
 
-const browser = await puppeteer.launch({
-  executablePath: "/usr/bin/chromium-browser",
-  headless: "shell",
-  args: ["--no-sandbox"],
-  defaultViewport: {
-    width: 400,
-    height: 400,
-  },
-});
+async function getBrowser() {
+  return await puppeteer.launch({
+    executablePath: "/usr/bin/chromium-browser",
+    headless: "shell",
+    args: ["--no-sandbox"],
+    defaultViewport: {
+      width: 400,
+      height: 400,
+    },
+  });
+}
+let browser = await getBrowser();
 
 app.get("/screenshot/:url", async (req, res) => {
   try {
     const url = req.params.url;
 
     try {
+      if(!browser.connected) {
+        await browser.close();
+        browser = await getBrowser();
+      }
+
       const page = await browser.newPage();
       await page.goto(`https://${url}`);
 
